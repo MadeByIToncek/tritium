5e1adcd591de97534af55f52551f3a2528a9752e
Made By IToncek <toncek2019@post.cz>
2024-02-10 15:04:18 +0100

    Switch to FluentNHibernate

--- client/DatabaseTableTemplates.cs
+++ client/DatabaseTableTemplates.cs
@@ -1,73 +1,97 @@
-﻿using SQLite;
+﻿using FluentNHibernate.Mapping;
 
 namespace Tritium
 {
-    [Table("tritium_clients")]
     public class Klient
     {
-        [PrimaryKey, AutoIncrement]
-        [Column("id")]
-        public required int Id { get; set; }
-        [Column("name")]
-        public required string Name { get; set; }
-        [Column("birthdate")]
-        public DateTime Birthdate { get; set; }
-        [Column("telephone")]
-        public string Telephone { get; set; }
-        [Column("email")]
-        public string Email { get; set; }
-        [Column("address")]
-        public string Address { get; set; }
-        [Column("initial_meeting")]
-        public DateTime InitialMeeting { get; set; }
-        [Column("kardiostimulator")]
+        #region Important
+        public virtual int Id { get; protected set; }
+        public virtual string Jmeno { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        public virtual string Telefon { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Adresa { get; set; }
+        public virtual DateTime UvodniSchuzka { get; set; }
+        public virtual IList<Navsteva> Navstevy { get; set; }
 
+        #endregion
+        #region Details
         //Detailed stuff
 
-        public bool Kardiostimulator { get; set; }
-        [Column("aktualni_tehotenství")]
-        public bool AktualniTehotenstvi { get; set; }
-        [Column("onkologicke_onemocneni")]
-        public bool OnkologickeOnemocneni { get; set; }
-        [Column("epilepsie")]
-        public bool Epilepsie { get; set; }
-        [Column("autoimunitni_onemocneni")]
-        public bool AutoimunitniOnemocneni { get; set; }
-        [Column("leky")]
-        public bool Leky { get; set; }
-        [Column("dlouhodobe_potize")]
-        public bool DlouhodobePotize { get; set; }
-        [Column("krevni_tlak")]
-        public bool KrevniTlak { get; set; }
-        [Column("predchazejici_nemoci")]
-        public bool PredchazejiciNemoci { get; set; }
-        [Column("homeo")]
-        public bool Homeo { get; set; }
-        [Column("rodinna_anamneza")]
-        public bool RodinnaAnamneza { get; set; }
-        [Column("strava")]
-        public bool Strava { get; set; }
-        [Column("traveni")]
-        public bool Traveni { get; set; }
-        [Column("situace_prace")]
-        public bool SituacePrace { get; set; }
-        [Column("situace_rodina")]
-        public bool SituaceRodina { get; set; }
-        [Column("situace_ostatni")]
-        public bool SituaceOstatni { get; set; }
-        [Column("rozpolozeni_klienta")]
-        public bool Rozpolozeni { get; set; }
+        public virtual bool Kardiostimulator { get; set; }
+        public virtual bool AktualniTehotenstvi { get; set; }
+        public virtual bool OnkologickeOnemocneni { get; set; }
+        public virtual bool Epilepsie { get; set; }
+        public virtual bool AutoimunitniOnemocneni { get; set; }
+        public virtual bool Leky { get; set; }
+        public virtual bool DlouhodobePotize { get; set; }
+        public virtual bool KrevniTlak { get; set; }
+        public virtual bool PredchazejiciNemoci { get; set; }
+        public virtual bool Homeo { get; set; }
+        public virtual bool RodinnaAnamneza { get; set; }
+        public virtual bool Strava { get; set; }
+        public virtual bool Traveni { get; set; }
+        public virtual bool SituacePrace { get; set; }
+        public virtual bool SituaceRodina { get; set; }
+        public virtual bool SituaceOstatni { get; set; }
+        public virtual bool Rozpolozeni { get; set; }
+        #endregion
+
+
+        public virtual void PridatNavstevu(Navsteva navsteva)
+        {
+            Navstevy.Add(navsteva);
+        }
     }
 
-    [Table("tritium_visits")]
+
+    public class MapaKlientu : ClassMap<Klient>
+    {
+        public MapaKlientu()
+        {
+            Id(x => x.Id);
+            Map(x => x.Jmeno);
+            Map(x => x.DatumNarozeni);
+            Map(x => x.Telefon);
+            Map(x => x.Email);
+            Map(x => x.Adresa);
+            Map(x => x.UvodniSchuzka);
+            HasMany(x => x.Navstevy)
+                .Inverse()
+                .Cascade.All();
+            Map(x => x.Kardiostimulator);
+            Map(x => x.AktualniTehotenstvi);
+            Map(x => x.OnkologickeOnemocneni);
+            Map(x => x.Epilepsie);
+            Map(x => x.AutoimunitniOnemocneni);
+            Map(x => x.Leky);
+            Map(x => x.DlouhodobePotize);
+            Map(x => x.KrevniTlak);
+            Map(x => x.PredchazejiciNemoci);
+            Map(x => x.Homeo);
+            Map(x => x.RodinnaAnamneza);
+            Map(x => x.Strava);
+            Map(x => x.Traveni);
+            Map(x => x.SituacePrace);
+            Map(x => x.SituaceRodina);
+            Map(x => x.SituaceOstatni);
+            Map(x => x.Rozpolozeni);
+        }
+    }
+
     public class Navsteva
     {
-        [PrimaryKey,AutoIncrement,Unique,NotNull]
-        [Column("id")]
-        public required int Id { get; set; }
-        [Column("client"),NotNull]
-        public required int Client { get; set; }
-        [Column("date"),NotNull]
-        public required DateTime Date { get; set; }
+        public virtual int Id { get; protected set; }
+        public virtual Klient Client { get; set; }
+        public virtual DateTime Date { get; set; }
+    }
+    public class MapaNavstev : ClassMap<Navsteva>
+    {
+        public MapaNavstev() {
+
+            Id(x => x.Id);
+            References(x => x.Client);
+            Map(x => x.Date);
+        }
     }
 }
